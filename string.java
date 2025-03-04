814932ae8ff04a5b56580ea67802747d414bff98
Sumangal Karan <123644743+Sumangal44@users.noreply.github.com>
2025-03-04 20:43:03 +0530

    Enhance variables.java with additional variable types and improve comments; add string.java to demonstrate string concatenation

---
+++ string.java
@@ -0,0 +1,11 @@
+public class string {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+        String name2 = "Doe";
+        String name3 = name + name2;
+        System.out.println(name3);
+        System.out.println("Hello " + name + " " + name2);
+        System.out.println("Hello " + name + " " + name2 + " " + 5);
+    }
+}
--- variables.java
+++ variables.java
@@ -1,18 +1,25 @@
 public class variables {
     public static void main(String[] args) {
-        int myNum = 5;               // Integer (whole number)
-        float myFloatNum = 5.99f;    // Floating point number
-        char myLetter = 'D';         // Character
-        boolean myBool = true;       // Boolean
-        String myText = "Hello";     // String
+        int myNum = 5;
+        byte mynum = 100;
+        // Integer (whole number)
+        float myFloatNum = 5.99f; // Floating point number
+        char myLetter = 'D'; // Character
+        boolean myBool = true; // Boolean
+        String myText = "Hello";
+        // String
 
         System.out.println(myNum);
         System.out.println(myFloatNum);
         System.out.println(myLetter);
         System.out.println(myBool);
         System.out.println(myText);
+        System.out.println(mynum);
 
-        // You can also declare a variable without assigning the value, and assign the value later:
+        short myum = 5000;
+        System.out.println(myum);
+        // You can also declare a variable without assigning the value, and assign the
+        // value later:
         int myNum1;
         myNum1 = 15;
         System.out.println(myNum1);
@@ -20,9 +27,13 @@ public class variables {
         // Final Variables
         // To create a constant, use the final keyword.
         final int myNum2 = 15;
-        // myNum2 = 20; // will generate an error: cannot assign a value to a final variable
+        // myNum2 = 20; // will generate an error: cannot assign a value to a final
+        // variable
         System.out.println(myNum2);
-        
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
 
     }
 }
