f7c37e4c88fe49aa5ae0ea4a87abf3c60b52932b
Sumangal Karan <123644743+Sumangal44@users.noreply.github.com>
2025-03-04 20:30:06 +0530

    Add basic arithmetic operations example in number.java

---
+++ number.java
@@ -0,0 +1,11 @@
+public class number {
+    public static void main(String[] args) {
+        System.out.println(4);
+        System.out.println(4 + 4);
+        System.out.println(4 - 4);
+        System.out.println(4 * 4);
+        System.out.println(4 / 4);
+        System.out.println(4 % 4);
+        
+    }
+}
