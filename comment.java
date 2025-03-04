150867a1b3eb4f83490b8f01e61a99b4f09b990b
Sumangal Karan <123644743+Sumangal44@users.noreply.github.com>
2025-03-04 20:31:56 +0530

    Add comment.java to demonstrate single and multi-line comments

---
+++ comment.java
@@ -0,0 +1,12 @@
+public class comment {
+    public static void main(String[] args) {
+        System.out.println("This is a comment");
+        // This is a single line comment
+        System.out.println("This is not a comment");
+        /* This is a multi-line comment
+        This is a multi-line comment
+        This is a multi-line comment
+        This is a multi-line comment
+        */
+    }
+}
